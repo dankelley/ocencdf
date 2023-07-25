9f30c6d61798da354b167c5814593d47bdfc2545
dankelley <kelley.dan@gmail.com>
2023-07-25 10:25:24 -0300

    web: mention all built-in vartables
    I had forgotten adp and adv.

--- vignettes/built_in_vartables.R
+++ vignettes/built_in_vartables.R
@@ -1,4 +1,20 @@
 ## ----eval=FALSE---------------------------------------------------------------
+#  system.file("extdata", "adp.yml", package="ocencdf")
+
+## ---- echo=FALSE--------------------------------------------------------------
+lines <- readLines(system.file("extdata", "adp.yml", package="ocencdf"))
+for (line in lines)
+    cat(line, "\n")
+
+## ----eval=FALSE---------------------------------------------------------------
+#  system.file("extdata", "adv.yml", package="ocencdf")
+
+## ---- echo=FALSE--------------------------------------------------------------
+lines <- readLines(system.file("extdata", "adv.yml", package="ocencdf"))
+for (line in lines)
+    cat(line, "\n")
+
+## ----eval=FALSE---------------------------------------------------------------
 #  system.file("extdata", "argo.yml", package="ocencdf")
 
 ## ---- echo=FALSE--------------------------------------------------------------

a6580be988350c5b3b7ab594402e2c9316192e64
dankelley <kelley.dan@gmail.com>
2024-02-21 20:14:21 -0400

    clean 'yhp' typo (for 'whp') throughout

--- vignettes/built_in_vartables.R
+++ vignettes/built_in_vartables.R
@@ -23,7 +23,7 @@ for (line in lines)
     cat(line, "\n")
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  system.file("extdata", "yhp.yml", package="ocencdf")
+#  system.file("extdata", "whp.yml", package="ocencdf")
 
 ## ----echo=FALSE---------------------------------------------------------------
 lines <- readLines(system.file("extdata", "whp.yml", package="ocencdf"))
